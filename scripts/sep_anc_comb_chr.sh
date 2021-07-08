bd9d120ce78c6e03c28ecdce2617269223055843
Mira Mastoras <mastorasmira@gmail.com>
2021-07-08 11:03:00 -0700

    Update sep_anc_comb_chr.sh

--- scripts/sep_anc_comb_chr.sh
+++ scripts/sep_anc_comb_chr.sh
@@ -2,11 +2,8 @@ DATASET=$1
 ANC=$2
 ADMIDS=$3
 
-# combine ancestries - old code from Brownings script compatible with HapIBD
-# for chr in `seq 1 22`; do cat results/IBD-segs/${DATASET}.chr${chr}.phased.filled.allanc.ibd | grep -i "[[:space:]]${ANC}"'$' | cut -f1-8 -d' ' > results/IBD-segs/${DATASET}.chr${chr}.phased.filled.anc${ANC}.ibd ; done
-
-# fixed code - compatible with refined-ibd
-for chr in `seq 1 22`; do cat results/IBD-segs/${DATASET}.chr${chr}.phased.filled.allanc.ibd | grep -i "[[:space:]]${ANC}"'$' | cut -f1-7,9 -d' ' > results/IBD-segs/${DATASET}.chr${chr}.phased.filled.anc${ANC}.ibd ; done
+# combine ancestries 
+for chr in `seq 1 22`; do cat results/IBD-segs/${DATASET}.chr${chr}.phased.filled.allanc.ibd | grep -i "[[:space:]]${ANC}"'$' | cut -f1-8 -d' ' > results/IBD-segs/${DATASET}.chr${chr}.phased.filled.anc${ANC}.ibd ; done
 
 # combine chroms
 cat results/IBD-segs/${DATASET}.chr*.phased.filled.anc${ANC}.ibd > results/IBD-segs/${DATASET}.phased.filled.anc${ANC}.ibd
